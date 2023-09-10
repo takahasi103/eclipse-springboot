040de608a40cd3dee811b1468cdd253bb20544c5
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-10 20:18:28 +0900

    Userクラス @Entity追記

--- src/main/java/com/example/demo/data/entity/User.java
+++ src/main/java/com/example/demo/data/entity/User.java
@@ -1,14 +1,19 @@
 package com.example.demo.data.entity;
 
+import javax.persistence.Entity;
 
 import lombok.Data;
 
+@Entity // このクラスをEntityとして使うためのアノテーション
 @Data // Getter、Setterを省略するためのLombokのアノテーション
 public class User {
 	
+	// userテーブルのid
 	private Long id;
 
+	// userテーブルのname
 	private String name;
 
+	// userテーブルのemail
 	private String email;
 }
