16c5ac8bfe10bb180e7c4ff27ec80d80bc1395fa
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-10 21:19:01 +0900

    UserRepositoryを使えるようにする

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.data.repository.UserRepository;
+
 @Controller
 public class UserController {
+	// @Autowiredアノテーションを付けると、Spring Bootが自動でインスタンスをInjectします。
+	@Autowired
+	private UserRepository userRepository;
+	
 	// @RequestMapping(path = "/user", method = RequestMethod.GET)の省略版。
 	// HTTPのメソッドGETのみ受け付けます。
 	@GetMapping("/users")
