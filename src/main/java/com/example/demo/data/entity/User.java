93538264e91e97be99064165f290f45a7fee48e1
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-10 20:20:51 +0900

    Userクラス Entityクラスは完成

--- src/main/java/com/example/demo/data/entity/User.java
+++ src/main/java/com/example/demo/data/entity/User.java
@@ -1,6 +1,9 @@
 package com.example.demo.data.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
@@ -8,6 +11,8 @@ import lombok.Data;
 @Data // Getter、Setterを省略するためのLombokのアノテーション
 public class User {
 	
+	@Id // userテーブルのプライマリーキーidに付けるアノテーション
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // idがMySQLのauto_incrementの場合、自動生成させるためにアノテーションを付ける
 	// userテーブルのid
 	private Long id;
 
