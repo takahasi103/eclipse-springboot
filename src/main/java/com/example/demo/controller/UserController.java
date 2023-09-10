2ba8b8ef90add7319cfab917c878f6e451739e77
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-10 21:16:01 +0900

    メソッド作成

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+	// @RequestMapping(path = "/user", method = RequestMethod.GET)の省略版。
+	// HTTPのメソッドGETのみ受け付けます。
+	@GetMapping("/users")
+	public String getUsers() {
 
+		// テンプレートは src/main/resources/templates/users.html とします。
+		return "users";
+	}
 }
