18a8f4ea83940b2604a1d55c67f56acf30f6a4b1
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-10 20:16:19 +0900

    Userクラス作成

--- src/main/java/com/example/demo/data/entity/User.java
+++ src/main/java/com/example/demo/data/entity/User.java
@@ -1,5 +1,14 @@
 package com.example.demo.data.entity;
 
-public class User {
 
+import lombok.Data;
+
+@Data // Getter、Setterを省略するためのLombokのアノテーション
+public class User {
+	
+	private Long id;
+
+	private String name;
+
+	private String email;
 }
