a367d523c9b0a54e6315a81af16bce77dc1e2380
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-10 20:27:30 +0900

    org.springframework.data.jpa.repository.JpaRepositoryを継承

--- src/main/java/com/example/demo/data/repository/UserRepository.java
+++ src/main/java/com/example/demo/data/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.data.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.data.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
