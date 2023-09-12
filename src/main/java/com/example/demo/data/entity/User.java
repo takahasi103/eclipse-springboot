3115df01a54cad16f0e1c2a5a75b136eaa36b130
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-12 21:39:35 +0900

    登録日の表示を変更

--- src/main/java/com/example/demo/data/entity/User.java
+++ src/main/java/com/example/demo/data/entity/User.java
@@ -1,6 +1,8 @@
 package com.example.demo.data.entity;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -30,4 +32,11 @@ public class User {
 	@CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createTime;
+	
+	// 日本時間の年月日にフォーマットするメソッド
+	public String getFormattedCreateTime() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日")
+				.withZone(ZoneId.of("Asia/Tokyo")); // 日本のタイムゾーンに設定
+		return createTime.format(formatter);
+	}
 }
